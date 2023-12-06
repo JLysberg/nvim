3013e5ab5ec8f7c9753ce5b362472a9d7b2530a6
JLysberg <jostein.lysberg@gmail.com>
2023-12-06 11:26:50 +0100

    feat: change cwd on nvimtree open

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -16,4 +16,20 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+-- change cwd on nvim-tree open
+local function open_nvim_tree(data)
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
 
+  if not directory then
+    require("nvim-tree.api").tree.open()
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
