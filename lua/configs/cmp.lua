29ab7611603c1e4cabf90459f94d88e946865ade
JLysberg <jostein.lysberg@gmail.com>
2025-05-05 12:39:30 +0200

    feat: rework cmp mappings

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -1,31 +1,56 @@
+local luasnip = require "luasnip"
 local cmp = require "cmp"
+
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
 
 local options = {
   mapping = {
-    ["<Down>"] = cmp.mapping(function(fallback)
+    ["<CR>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
-        cmp.select_next_item()
-      elseif require("luasnip").expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+        if luasnip.expandable() then
+          luasnip.expand()
+        else
+          cmp.confirm {
+            select = true,
+          }
+        end
       else
         fallback()
       end
-    end, {
-      "i",
-      "s",
-    }),
-    ["<Up>"] = cmp.mapping(function(fallback)
+    end),
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        if #cmp.get_entries() == 1 then
+          cmp.confirm { select = true }
+        else
+          cmp.select_next_item()
+        end
+      elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
+      elseif has_words_before() then
+        cmp.complete()
+        if #cmp.get_entries() == 1 then
+          cmp.confirm { select = true }
+        end
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif require("luasnip").jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
       else
         fallback()
       end
-    end, {
-      "i",
-      "s",
-    }),
+    end, { "i", "s" }),
   },
 }
 
