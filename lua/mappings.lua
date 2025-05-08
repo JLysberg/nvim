ecf420cb088bbdc0f7532558ad2cb2f3b179a1a7
JLysberg <jostein.lysberg@gmail.com>
2025-05-08 15:18:13 +0200

    feat: add qol mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,12 +10,13 @@ map("n", ";", ":", { desc = "Enter command mode" })
 map("t", "<esc>", vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true), { desc = "Escape terminal mode" })
 
 -- general
-map({ "n", "t" }, "<C-q>", function()
+map("n", "<C-q>", function()
   require("nvchad.tabufline").close_buffer()
   vim.cmd "q"
 end, { desc = "Close buffer and window" })
+map("n", "n", "nzz", { desc = "Next match and center" })
+map("n", "N", "Nzz", { desc = "Prev match and center" })
 
-map("n", "<leader>wt", "<cmd> Telescope keymaps <cr>", { desc = "Display full keymap overview (Telescope)" })
 map(
   "n",
   "<leader>s",
@@ -53,11 +54,33 @@ end, { desc = "Toggle zen mode" })
 -- map("n", "<C-s>", require("harpoon"):list():select(4), { desc = "HARPOON Select fourth Harpoon file"  })
 
 -- lsp
--- todo rename, reference etc. mappings
+map("n", "[d", function()
+  vim.diagnostic.jump { count = -1, float = true }
+end, { desc = "Previous diagnostic with float" })
 
--- git
+map("n", "]d", function()
+  vim.diagnostic.jump { count = 1, float = true }
+  vim.diagnostic.open_float { scope = "line" }
+end, { desc = "Next diagnostic with float" })
+
+map("n", "<leader>fl", function()
+  vim.diagnostic.open_float { scope = "line" }
+end, { desc = "Show diagnostics float" })
+
+map("n", "K", function()
+  vim.lsp.buf.hover { border = "rounded" }
+end, { desc = "LSP hover" })
+
+-- telescope
 map("n", "<leader>gc", "<cmd> Telescope git_commits <cr>", { desc = "Git commits (Telescope)" })
 map("n", "<leader>gs", "<cmd> Telescope git_status <cr>", { desc = "Git status (Telescope)" })
+map("n", "<leader>gs", "<cmd> Telescope git_status <cr>", { desc = "Git status (Telescope)" })
+map("n", "<leader>fk", "<cmd> Telescope keymaps <cr>", { desc = "Display full keymap overview (Telescope)" })
+map("n", "<leader>fd", "<cmd> Telescope lsp_definitions <cr>", { desc = "Telescope lsp definitions" })
+map("n", "<leader>fr", "<cmd> Telescope lsp_references <cr>", { desc = "Telescope lsp references" })
+map("n", "<leader>fD", "<cmd> Telescope diagnostics <cr>", { desc = "Telescope diagnostics" })
+
+-- git
 map("n", "<leader>gb", function()
   require("gitsigns").blame_line()
 end, { desc = "Git blame" })
