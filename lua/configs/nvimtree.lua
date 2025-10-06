35c0fe15acb53d1fda9e283d357be29fcac699fe
JLysberg <jostein.lysberg@gmail.com>
2025-10-06 10:01:01 +0200

    feat: support nvimtree remapping

--- lua/configs/nvimtree.lua
+++ lua/configs/nvimtree.lua
@@ -4,6 +4,13 @@ local options = {
       quit_on_open = true,
     },
   },
+  on_attach = function(bufnr)
+    local api = require "nvim-tree.api"
+
+    api.config.mappings.default_on_attach(bufnr)
+
+    vim.keymap.del("n", "E", { buffer = bufnr })
+  end,
 }
 
 return options
