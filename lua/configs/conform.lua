e1ded408d155b2f8db56bd543add20982574d18b
JLysberg <jostein.lysberg@gmail.com>
2026-01-07 10:38:41 +0100

    fix: add DOTNET_ROOT env

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -13,14 +13,6 @@ local options = {
     proto = { "buf" },
   },
 
-  formatters = {
-    -- fix until https://github.com/stevearc/conform.nvim/pull/695 is merged
-    csharpier = {
-      command = "csharpier",
-      args = { "format" },
-    },
-  },
-
   format_on_save = {
     timeout_ms = 1000,
     lsp_fallback = true,
--- lua/options.lua
+++ lua/options.lua
@@ -9,3 +9,13 @@ o.cursorlineopt = "number"
 o.relativenumber = true
 
 vim.notify = require "notify"
+
+-- Ensure .NET apphosts can locate the system runtime when Neovim is started from an environment that lacks DOTNET_ROOT.
+local dotnet_root = "/usr/share/dotnet"
+if vim.fn.isdirectory(dotnet_root) == 1 then
+  vim.env.DOTNET_ROOT = dotnet_root
+end
+local dotnet_tools = vim.fn.expand "$HOME/.dotnet/tools"
+if vim.fn.isdirectory(dotnet_tools) == 1 and not string.find(vim.env.PATH or "", dotnet_tools, 1, true) then
+  vim.env.PATH = dotnet_tools .. ":" .. (vim.env.PATH or "")
+end
